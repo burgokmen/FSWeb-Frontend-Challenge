2595441e041a11fc841f9e208f40f77e70f82acb
burgokmen <burgokmen@gmail.com>
2023-09-08 18:08:40 +0300

    assets added

--- Gokmen-Resume/src/App.jsx
+++ Gokmen-Resume/src/App.jsx
@@ -1,10 +1,8 @@
 function App() {
   return (
     <>
-      <div>
-        <div className="text-3xl font-bold">Gökmen</div>
-      </div>
-      <header></header>
+      <div></div>
+      <header className="text-3xl font-bold">Gökmen</header>
       <div className="hero"></div>
 
       <div className="profile"></div>
