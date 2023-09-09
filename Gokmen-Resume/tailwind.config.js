4a9b29a5954686cd2e8682668da41864b70bdf88
burgokmen <burgokmen@gmail.com>
2023-09-10 01:45:29 +0300

    Options component dark mode added.

--- Gokmen-Resume/tailwind.config.js
+++ Gokmen-Resume/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        13: "3.25rem",
+        15: "3.75rem",
+        128: "247px",
+        144: "36rem",
+      },
+    },
   },
   plugins: [],
 };
