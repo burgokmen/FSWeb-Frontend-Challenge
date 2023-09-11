7651663b5db71443d23f6962424a0e4eb2ab06c7
burgokmen <burgokmen@gmail.com>
2023-09-12 00:21:09 +0300

    Project Comp done with 2 todos

--- Gokmen-Resume/src/components/Projects.jsx
+++ Gokmen-Resume/src/components/Projects.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import heroPhoto from "../assets/project-photo.png";
 import { buttonArr, projectsData } from "../data";
 import { MapContext } from "../contexts/MapProvider";
+//TODO border radius need to added + shadow can be added lin18
+//TODO real project photos and links need to be added
 
 export default function Projects() {
   const { buttonFunc } = useContext(MapContext);
@@ -14,8 +16,8 @@ export default function Projects() {
       {projectsData.map((pr, i) => {
         return (
           <div key={i} className="flex-col pb-10">
-            <div className="flex justify-center">
-              <img className="w-[20rem] h-[20rem]" src={heroPhoto} />
+            <div className="flex justify-center ">
+              <img className="w-[20rem] h-[20rem] " src={heroPhoto} />
               <div className="w-[22rem] h-[20rem] p-6 bg-white flex-col ">
                 <h3 className="font-bold text-[1.6rem] pb-[1.3rem]">{pr.h3}</h3>
                 <p className="font-normal text-[0.8rem] pb-[1.3rem]">{pr.p}</p>
