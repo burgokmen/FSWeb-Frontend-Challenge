16dbb94e14e84794fb4f7107c0219eb0f2158a2a
burgokmen <burgokmen@gmail.com>
2023-09-12 00:04:51 +0300

    Projects map done

--- Gokmen-Resume/src/components/Projects.jsx
+++ Gokmen-Resume/src/components/Projects.jsx
@@ -11,28 +11,31 @@ export default function Projects() {
       <h2 className="px-[7.5rem] py-[3rem] text-mypurp-1 text-[3rem] font-interbold font-bold">
         Projects
       </h2>
-      <div className="flex-col pb-10">
-        <div className="flex justify-center">
-          <img className="w-[20rem] h-[20rem]" src={heroPhoto} />
-          <div className="w-[22rem] h-[20rem] p-6 bg-white flex-col ">
-            <h3 className="font-bold text-[1.6rem] pb-[1.3rem]">
-              {projectsData[0].h3}
-            </h3>
-            <p className="font-normal text-[0.8rem] pb-[1.3rem]">
-              {projectsData[0].p}
-            </p>
-            <div className="pb-[1.3rem]">{buttonFunc(buttonArr)}</div>
-            <div className="flex">
-              <a className="pr-2 underline" href="https://www.w3schools.com/">
-                View Site
-              </a>
-              <a className="underline" href="https://www.w3schools.com/">
-                Github
-              </a>
+      {projectsData.map((pr, i) => {
+        return (
+          <div key={i} className="flex-col pb-10">
+            <div className="flex justify-center">
+              <img className="w-[20rem] h-[20rem]" src={heroPhoto} />
+              <div className="w-[22rem] h-[20rem] p-6 bg-white flex-col ">
+                <h3 className="font-bold text-[1.6rem] pb-[1.3rem]">{pr.h3}</h3>
+                <p className="font-normal text-[0.8rem] pb-[1.3rem]">{pr.p}</p>
+                <div className="pb-[1.3rem]">{buttonFunc(buttonArr)}</div>
+                <div className="flex">
+                  <a
+                    className="pr-2 underline"
+                    href="https://www.w3schools.com/"
+                  >
+                    View Site
+                  </a>
+                  <a className="underline" href="https://www.w3schools.com/">
+                    Github
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
