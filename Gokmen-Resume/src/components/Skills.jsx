9b3220bb8f8df33b67971ab601cd263ed07acc2f
burgokmen <burgokmen@gmail.com>
2023-09-15 20:01:27 +0300

    bugfix

--- Gokmen-Resume/src/components/Skills.jsx
+++ Gokmen-Resume/src/components/Skills.jsx
@@ -11,7 +11,7 @@ export default function Skills() {
   return (
     <div className="dark:bg-mydark-3">
       <h2 className="font-interreg dark:text-mygreen-1 font-bold text-mypurp-1 text-[3rem] px-[16.66%] py-[3rem]  ">
-        {data[language].skills.skils}
+        {data[language].skills.skills}
       </h2>
       <div className="flex justify-evenly py-10 items-baseline dark:text-white">
         <div className="flex">
